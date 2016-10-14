5f86c7716387459d4460d8317b7fc1f7a5cdfadc
vishal mehra <vishal.me28@gmail.com>
2016-10-14 20:50:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 app.get('/article-one',function(req, res){
     res.sendFile('article had been server');
 });
@@ -17,10 +22,6 @@ app.get('/article-two',function(req, res){
 
 app.get('/article-three',function(req, res){
     res.sendFile('article three had been server');
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
