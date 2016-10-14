30dd775b6cca086cdb4405af56a07e9c702a4f99
vishal mehra <vishal.me28@gmail.com>
2016-10-15 03:45:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.send('article had been server');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
 
